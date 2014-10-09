c3f049dd54e0f4e3bfe4520e7371f0a520e241ef
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-09 22:10:36 +0400

    implement lst:delete

--- src/lst.erl
+++ src/lst.erl
@@ -14,8 +14,23 @@ insert(L, Val) ->
     [Val | L].
 
 delete(L, Val) ->
-    L.
+    delete(new(), L, Val).
+
+delete(Start, [], _) ->
+    reverse(Start);
+delete(Start, [Val | T], Val) ->
+    concat(reverse(Start), T);
+delete(Start, [H | T], Val) ->
+    delete(insert(Start, H), T, Val).
 
 lookup(L, Val) ->
     false.
+
+reverse(L) ->
+    %% stub
+    lists:reverse(L).
+
+concat(L1, L2) ->
+    %% stub
+    L1 ++ L2.
 
