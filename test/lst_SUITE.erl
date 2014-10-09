57fa41f4fc44b13df06a00a1a43cc036d41391fc
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-09 21:55:02 +0400

    add tests

--- test/lst_SUITE.erl
+++ test/lst_SUITE.erl
@@ -5,9 +5,33 @@
 -compile(export_all).
 
 all() -> [
-          lst
+          {group, lst}
          ].
 
-lst(_Config) ->
+groups() ->
+    [
+     {lst, [], [
+                new,
+                insert,
+                delete,
+                lookup
+               ]}
+    ].
+
+new(_Config) ->
+    [] = lst:new().
+
+insert(_Config) ->
+    L0 = lst:new(),
+    L1 = lst:insert(L0, 1),
+    L1 = [1],
+    L2 = lst:insert(L1, 2),
+    L2 = [2,1],
+    ok.
+
+delete(_Config) ->
+    ok.
+
+lookup(_Config) ->
     ok.
 
