61eaae7224b7dd9835550be6424f466756c9758b
Arkadiy Drovosekov <arkdro@gmail.com>
2014-10-09 22:22:17 +0400

    use own code for delete instead of pattern matching

--- src/lst.erl
+++ src/lst.erl
@@ -16,12 +16,22 @@ insert(L, Val) ->
 delete(L, Val) ->
     delete(new(), L, Val).
 
-delete(Start, [], _) ->
-    reverse(Start);
-delete(Start, [Val | T], Val) ->
-    concat(reverse(Start), T);
-delete(Start, [H | T], Val) ->
-    delete(insert(Start, H), T, Val).
+delete(Start, L, Val) ->
+    case is_empty(L) of
+        true ->
+            reverse(Start);
+        false ->
+            delete_from_non_empty(Start, L, Val)
+    end.
+
+delete_from_non_empty(Start, L, Val) ->
+    Head = head(L),
+    Tail = tail(L),
+    if Head =:= Val ->
+            concat(reverse(Start), Tail);
+       true ->
+            delete(insert(Start, Head), Tail, Val)
+    end.
 
 lookup(L, Val) ->
     false.
@@ -33,4 +43,15 @@ reverse(L) ->
 concat(L1, L2) ->
     %% stub
     L1 ++ L2.
+
+is_empty([]) ->
+    true;
+is_empty(_) ->
+    false.
+
+head([H | _]) ->
+    H.
+
+tail([_ | T]) ->
+    T.
 
